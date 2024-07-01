0af1e8e3e1068b2c147ec5d2fd8b2807c42ea4fb
NathanThom <nathanthom@nevada.unr.edu>
2024-07-01 13:50:02 -0500

    Rename directory and remove unintended duplicate files.

--- SmartRecon/Network Traffic Fingerprinting for IoT Device Identification/README.md
+++
@@ -1,5 +0,0 @@
-# SmartRecon - Network Traffic Fingerprinting for IoT Device Identification
-Link to paper:
-Link to data:
-  Link to uncleaned: https://drive.google.com/drive/folders/1rskr-OvccrrwGokDnMWjsfpxsiu6v-BI?usp=sharing
-  Link to cleaned: https://drive.google.com/drive/folders/1E6QZspr9T2qqtilZzACCqsJUzrnhc0UZ?usp=sharing
--- SmartRecon/Network_Traffic_Fingerprinting_for_IoT_Device_Identification/hash_code/split.py
+++ SmartRecon/Network_Traffic_Fingerprinting_for_IoT_Device_Identification/hash_code/split.py
@@ -1,33 +1,25 @@
 import os
+import os.path
 from itertools import combinations
 import itertools
-import numpy as np
-import os
-import os.path
 import random
 import subprocess
 import sys
 import time
 
+import numpy as np
+
 
 def splitter(input_pcap, outputbase, increment):
-
     command = "tshark -r " + input_pcap + " -T fields -e frame.time_relative"
     proc = subprocess.Popen([command], stdout=subprocess.PIPE, shell=True)
     (out, err) = proc.communicate()
-    # print(out)
-
     splitted = out.split(b"\n")
     last_second = int(float(splitted[-2]))
-    # window_insecond = 60*10
-    # window_insecond = 60 * increment
-    window_insecond = 60 * 1
 
+    window_insecond = 60 * 1
     counter = 1
     if last_second > window_insecond:
-
-        # for i in range(0, last_second, 60*10):
-        # for i in range(0, last_second, 60 * increment):
         for i in range(0, last_second, 60 * 1):
 
             start_time = i
@@ -41,16 +33,14 @@ def splitter(input_pcap, outputbase, increment):
             )
 
             command = "tshark -r " + input_pcap
-            command = command + " -Y 'frame.time_relative >= "
-            command = command + str(start_time)
-            command = command + " and frame.time_relative <= "
-            command = command + str(end_time) + "' -w "
-            command = command + output_pcap
+            command += " -Y 'frame.time_relative >= "
+            command += +str(start_time)
+            command += +" and frame.time_relative <= "
+            command += str(end_time) + "' -w "
+            command += output_pcap
 
             os.system(command)
 
-            # if((end_time + 60*10) > last_second):
-            # if((end_time + 60 * increment) > last_second):
             if (end_time + 60 * 1) > last_second:
                 break
             counter = counter + 1
@@ -58,29 +48,32 @@ def splitter(input_pcap, outputbase, increment):
     print("finished", input_pcap)
 
 
-# base = '/home/jay/Desktop/10-minute/'
-# for folder in os.listdir(base):
-# 	for file in os.listdir(base + '/' + folder):
+def main():
+    parser = argparse.ArgumentParser(
+        description="Split pcapng files into subfiles containing the packets which occur in a n-second time window."
+    )
+    parser.add_argument(
+        "inbase",
+        type=str,
+        help="Path to original files to be cleaned. Directory should be organized like inbase/files.",
+    )
+    parser.add_argument(
+        "outbase",
+        type=str,
+        help="Path to save cleaned files.",
+    )
+    args = parser.parse_args()
 
-# 		outputbase = "/home/jay/Desktop/increments_9/" + folder + '/' + file.split("_")[0]
-# 		print(outputbase)
+    inpath = args.inbase
+    outpath = args.outbase
+    counter = 10
 
-# 		# path_to_file = base + '/' + folder + '/' + file
+    for folder in os.listdir(inpath):
+        for file in os.listdir(inpath + folder):
+            input_pcap = inpath + folder + "/" + file
+            outputbase = outpath + f"{counter}-minute/" + folder + "/"
+            splitter(input_pcap, outputbase, counter)
 
-# 		# print(path_to_file)
-# 		# splitter(path_to_file, outputbase)
 
-inpath = "/home/jay/Desktop/24-hour/"
-# outpath = '/home/jay/Desktop/10-minute/'
-outpath = "/home/jay/Desktop/"
-counter = 10
-
-# while counter > 0:
-for folder in os.listdir(inpath):
-    for file in os.listdir(inpath + folder):
-        input_pcap = inpath + folder + "/" + file
-        # outputbase = outpath + str(counter) + '-minute/' + folder + '/'
-        outputbase = outpath + "1-minute/" + folder + "/"
-        splitter(input_pcap, outputbase, counter)
-        # print(input_pcap, outputbase)
-    # counter -= 1
+if __name__ == "__main__":
+    main()
